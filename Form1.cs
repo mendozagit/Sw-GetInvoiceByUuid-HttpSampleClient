90b4c1e1f6b6e514c3039b7b60eca6082fd07e6a
JesúsMendoza <mendoza.git@gmail.com>
2022-11-28 19:31:27 -0600

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using static System.Windows.Forms.Design.AxImporter;
+using System.Text.Unicode;
+using SW_APIS_CLIENT.Models;
+
+namespace SW_APIS_CLIENT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetInvoiceByUuidButton_Click(object sender, EventArgs e)
+        {
+            var uri = @"http://api.test.sw.com.mx/datawarehouse/v1/live/246b0828-298e-4f30-bac5-10c1056d61f2";
+            var token =
+                "T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbXB3YVZxTHdOdHAwVXY2NTdJb1hkREtXTzE3dk9pMmdMdkFDR2xFWFVPUXpTUm9mTG1ySXdZbFNja3FRa0RlYURqbzdzdlI2UUx1WGJiKzViUWY2dnZGbFloUDJ6RjhFTGF4M1BySnJ4cHF0YjUvbmRyWWpjTkVLN3ppd3RxL0dJPQ.T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbFlVcU92YUJTZWlHU3pER1kySnlXRTF4alNUS0ZWcUlVS0NhelhqaXdnWTRncklVSWVvZlFZMWNyUjVxYUFxMWFxcStUL1IzdGpHRTJqdS9Zakw2UGRnSkpzVGJmUmJtMnkrRVVIWnJrWnlvZXBjV2twVXR3WEUrM2ZlY2F5QzBCL3JxZFduV0FMY3lDTnlQR3RONHcvS1VQcEVEblYzd0pZSUwvZ0Z6RnVIbkcreklaZE03RHl2eVJTMWkyQ2tpOHFyM2cxc2txOUlsQTdtTjNiYWd2dnN3R09GamJYL3Ywdk9kcTF4K0p2SE9pN1J3ZUJPcmFXcXpFUnUwOURiMHV4NGEwTHp0WkZabjV0UmxsUVlSM2pzQk1HNGI2dU4zWFFlU1ZCdWVoYkVOcjB1cXd5OGNENTVHdW1oL0VEVEQ5M3UyclFNMmpTNmtzQitJa0hEa2tiOTM4cWVHQ2F0bXJqRyt2Y2psQXdkUmc0TjRjbHY3S1hRTXdUelVabkIvME1PRmlPTE9UNFl6QnNaOU1KWDRIa252WUFNbWU3K3FHa0xSbzBkSHJzb0ViYVlwZGVzZlpBY1ppQzZoZG81NU11elJMdXpVUm02bEs0K2RoK204YVM.92uuBpBJP9rgFf8694IodP3iPQuhS9yYwqGvD4CpysM";
+
+            var httpClient = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            request.Headers.Add("Accept", "application/json");
+            var response = await httpClient.SendAsync(request);
+
+
+            var responseObject =  await response.Content.ReadFromJsonAsync<StorageResponse>(); //Error caracteres escapados que deforman el JSON.
+
+            var responseBody = response.Content.ReadAsStringAsync().Result;
+            var jsonResponse = JsonSerializer.Serialize(responseBody);
+
+            //await File.WriteAllTextAsync("InvoiceByUuidResponse.json", jsonResponse); // Al escribir el JSON se pueden ver caracteres escapados que deforman el JSON.
+
+
+
+            responseObject = JsonSerializer.Deserialize<StorageResponse>(jsonResponse);
+
+            MessageBox.Show($"StatusCode {response.StatusCode} reason {response.ReasonPhrase}");
+        }
+    }
+}
---
+++ Models/Class1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW_APIS_CLIENT.Models
+{
+    public class StorageResponse 
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+        public string messageDetail { get; set; }
+
+        public Data data { get; set; }
+    }
+    public partial class Data
+    {
+        
+        public MetaData metaData { get; set; }
+        
+        public List<Records> records { get; set; }
+    }
+    public partial class Records
+    {
+        
+        public string status { get; set; }
+
+        
+        public string urlPdf { get; set; }
+        
+        public string urlXml { get; set; }
+        
+        public bool hasAddenda { get; set; }
+        
+        public object addenda { get; set; }
+        
+        public string idDealer { get; set; }
+        
+        public string idUser { get; set; }
+        
+        public string version { get; set; }
+        
+        public string serie { get; set; }
+        
+        public string folio { get; set; }
+        
+        public DateTime fecha { get; set; }
+        
+        public string numeroCertificado { get; set; }
+        
+        public double subtotal { get; set; }
+        
+        public double descuento { get; set; }
+        
+        public double total { get; set; }
+        
+        public string moneda { get; set; }
+        
+        public double tipoCambio { get; set; }
+        
+        public string tipoDeComprobante { get; set; }
+        
+        public string metodoPago { get; set; }
+        
+        public string formaPago { get; set; }
+        
+        public string condicionesPago { get; set; }
+        
+        public string luegarExpedicion { get; set; }
+        
+        public string rfcEmisor { get; set; }
+        
+        public string nombreEmisor { get; set; }
+        
+        public string regimenFiscal { get; set; }
+        
+        public string rfcReceptor { get; set; }
+        
+        public string nombreReceptor { get; set; }
+        
+        public string residenciaFiscal { get; set; }
+        
+        public string numRegIdTrib { get; set; }
+        
+        public string usoCFDI { get; set; }
+        
+        public double totalImpuestosTraslados { get; set; }
+        
+        public double totalImpuestosRetencion { get; set; }
+        
+        public double trasladosIVA { get; set; }
+        
+        public double trasladosIEPS { get; set; }
+        
+        public double retencionesISR { get; set; }
+        
+        public double retencionesIVA { get; set; }
+        
+        public double retencionesIEPS { get; set; }
+        
+        public double totalImpuestosLocalesTraslados { get; set; }
+        
+        public double totalImpuestosLocalesRetencion { get; set; }
+        
+        public string complementos { get; set; }
+        
+        public string uuid { get; set; }
+        
+        public DateTime fechaTimbrado { get; set; }
+        
+        public string rfcProvCertif { get; set; }
+        
+        public string selloCFD { get; set; }
+        
+        public string urlAckCfdi { get; set; }
+        
+        public string urlAckCancellation { get; set; }
+        
+        public string urlAddenda { get; set; }
+        
+        public DateTime? fechaGeneracionPdf { get; set; }
+        
+        public string emisorRfc { get; set; }
+        
+        public string emisorNombre { get; set; }
+        
+        public string receptorRfc { get; set; }
+        
+        public string receptorNombre { get; set; }
+
+    }
+    public partial class MetaData
+    {
+        
+        public string page { get; set; }
+        
+        public string perPage { get; set; }
+        
+        public string pageCount { get; set; }
+        
+        public string totalCount { get; set; }
+        
+        public Links links { get; set; }
+    }
+    public partial class Links
+    {
+        public string current { get; set; }
+    }
+}
